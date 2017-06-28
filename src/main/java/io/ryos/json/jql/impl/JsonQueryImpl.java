0efa53440ced3f204786aa1a35a1b6226df6e37a
Bagdemir <bagdemir@adobe.com>
2017-06-28 13:07:58 +0200

    Minor refactorings.

--- src/main/java/io/ryos/json/jql/impl/JsonQueryImpl.java
+++ src/main/java/io/ryos/json/jql/impl/JsonQueryImpl.java
@@ -22,19 +22,25 @@ import io.ryos.json.jql.JQL;
 import io.ryos.json.jql.JsonQuery;
 import io.ryos.json.jql.TypeTransformer;
 import io.ryos.json.jql.exceptions.InvalidQuerySyntaxException;
+import io.ryos.json.jql.exceptions.InvalidTransformerException;
 import io.ryos.json.jql.transformers.ListTransformerImpl;
+
+import javax.json.JsonObject;
+import javax.json.JsonValue;
 import java.util.List;
 import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import javax.json.JsonObject;
-import javax.json.JsonValue;
 
 /**
+ * Json query implementation.
+ *
  * @author Erhan Bagdemir
+ * @since 1.0
  */
 public class JsonQueryImpl implements JsonQuery {
 
+  private static final String PATH_PTR = ".";
   /**
    * JSON object to query.
    */
@@ -49,25 +55,25 @@ public class JsonQueryImpl implements JsonQuery {
   public <T, E extends JsonValue> T query(String jql, TypeTransformer<T, E> transformer) {
     Objects.requireNonNull(jql, "JSON query must not be null.");
     Objects.requireNonNull(transformer, "Transformer must not be null.");
+
     checkSyntax(jql);
     final String[] segments = jql.substring(1).split(JQL.PATH_SEPERATOR);
+
     if (segments.length > 0) {
-      if (segments[0].startsWith(".")) {
+      if (segments[0].startsWith(PATH_PTR)) {
         segments[0] = segments[0].substring(1);
       }
       JsonObject current = jsonObject;
       for (int i = 0; i < segments.length; i++) {
         // Last item should be considered as value.
         if (i == segments.length - 1) {
-          String c_segment = segments[i];
+          final String c_segment = segments[i];
           if (isListQuery(c_segment)) {
-            if (!transformer.getClass().isAssignableFrom(ListTransformerImpl.class)) {
-              throw new RuntimeException("");
-            }
+            verifyListTransformer(transformer);
           } else if (c_segment.endsWith("]")) {
-            String index = c_segment.substring(c_segment.lastIndexOf("[")+1, c_segment.length()-1);
+            String index = c_segment.substring(c_segment.lastIndexOf("[") + 1, c_segment.length() - 1);
             String listQuery = c_segment.substring(0, c_segment.lastIndexOf("["));
-            List<T> query = query("."+listQuery, JsonQuery.asList(transformer));
+            List<T> query = query(PATH_PTR + listQuery, JsonQuery.asList(transformer));
             return query.get(Integer.valueOf(index));
           }
           return transformer.transform((E) current.get(segments[i]));
@@ -84,6 +90,14 @@ public class JsonQueryImpl implements JsonQuery {
     return null;
   }
 
+  private <T, E extends JsonValue> void verifyListTransformer(TypeTransformer<T, E> transformer) {
+    if (!transformer.getClass().isAssignableFrom(ListTransformerImpl.class)) {
+      throw new InvalidTransformerException(String.format("Invalid transformer: %s provided for expected: %s",
+              transformer.getClass(),
+              ListTransformerImpl.class));
+    }
+  }
+
   private boolean isListQuery(String segment) {
     return segment.endsWith("[]");
   }
@@ -92,7 +106,7 @@ public class JsonQueryImpl implements JsonQuery {
     boolean valid = false;
     if (query != null) {
       Pattern p = Pattern.compile(
-          "^(\\.)|(\\.((\\w+?(\\[\\d?\\])?)|(\".+\")))+|(\\.(((\\[\\d?\\])?)|(\".+\")))+$");
+              "^(\\.)|(\\.((\\w+?(\\[\\d?\\])?)|(\".+\")))+|(\\.(((\\[\\d?\\])?)|(\".+\")))+$");
       Matcher m = p.matcher(query);
       if (m.matches()) {
         valid = true;
